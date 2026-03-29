e5711fe16bedc194e8382ac11d9f7a0570cc5880
Harsh <harsharyahi@gmail.com>
2026-03-30 00:11:17 +0530

    create the login and signUp routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,42 +1,77 @@
 const express = require("express")
-const User = require("../models/user") 
+const User = require("../models/user")
 const bcrypt = require("bcrypt")
- 
-exports.signUp = async (req,res)=>{
-const {name, email , password , pic} = req.body
 
-if (!name || !email ||!password){
-    res.status(422).json({
-        msg : "all field required"
-    })
+exports.signUp = async (req, res) => {
+    const { name, email, password, pic } = req.body
+
+    if (!name || !email || !password) {
+        res.status(422).json({
+            msg: "all field required"
+        })
+    }
+
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+        res.status(400).json({
+            msg: "this email is already exist"
+        })
+    }
+    else {
+
+
+        const securePassword = await bcrypt.hash(password, 10)
+        // console.log(securePassword)
+
+        const user = new User({
+            name,
+            email,
+            password: securePassword,
+            pic
+        })
+
+        user.save()
+
+        res.status(200).json({
+            user
+        })
+    }
+
+
 }
 
-const userExist = await User.findOne({email})
+exports.logIn = async (req, res) => {
+    const { email, password } = req.body
 
-if (userExist) {
-    res.status(400).json({
-        msg : "this email is already exist"
-    })
-}
-else{
+    if (!email || !password) {
+        res.status(422).json({
+            msg: "all field required"
+        })
+    }
+    else {
+        const userExist = await User.findOne({ email })
+        if (!userExist) {
+            res.status(400).json({
+                msg: "this email don't exist"
+            })
+        }
 
+            const hashPassword = userExist.password
+            console.log(hashPassword)
 
-const securePassword = await bcrypt.hash( password, 10)
-// console.log(securePassword)
-
-const user = new User ({ 
-    name, 
-    email,
-    password : securePassword ,
-    pic
-})
-
-user.save()
-
-res.status(200).json({
-    user
-})
-}
-
+            const checkingPassword = await bcrypt.compare(password, hashPassword)
+            if (checkingPassword) {
+                res.status(200).json({
+                    name : userExist.name,
+                    msg : "login successfully"
+                })
+                // like this you mean by by return
+            } else {
+                res.status(422).json({
+                    msg: "User email or password is incorrect"
+                })
+            }
+    }
 
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const {signUp} = require("../controllers/authController")
+const {signUp , logIn} = require("../controllers/authController")
 
 router.post("/auth/signUp", signUp)
+router.post("/auth/login", logIn)
 
 module.exports = router
