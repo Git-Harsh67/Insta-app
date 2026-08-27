e061c1be8a65d6074d80a57bd085ab2d6352d2dd
Harsh <harsharyahi@gmail.com>
2026-08-27 22:54:08 +0530

    making ui better

--- FrontEnd/src/component/home page/Cards.jsx
+++ FrontEnd/src/component/home page/Cards.jsx
@@ -108,7 +108,7 @@ import React from "react";
 
 const Cards = (props) => {
   return (
-    <div className="w-140 rounded-md border-gray-500 text-white bg-gray-900 mt-6 mb-6 overflow-hidden ">
+    <div className="w-[40vw] rounded-md border-gray-500 text-white bg-gray-900 mt-6 mb-6 overflow-hidden ">
 
       <div className="flex items-center justify-between py-2 px-4">
         <div className="flex items-center gap-x-3">
--- FrontEnd/src/component/home page/Post.jsx
+++ FrontEnd/src/component/home page/Post.jsx
@@ -8,15 +8,26 @@ const Post = () => {
       <div className="h-screen ml-77 mt-12">
         <p className="text-white text-2xl mb-6">Create new post</p>
         {showImg === true && (
-          <img
-            className="w-100 h-80 border border-dashed border-gray-600 rounded-2xl "
-            src={imgUrl}
-            alt="post image"
-          />
+          <div >
+            <img
+              className="w-[30vw] border border-dashed border-gray-600 rounded-2xl object-fill"
+              src={imgUrl}
+              alt="post image"
+            />
+
+            <button
+              onClick={() => {
+                (setShowImg(false), setImgUrl(""));
+              }}
+              className="rounded-2xl bg-red-500 px-3 py-2 w-35 text-white mt-4"
+            >
+              Remove image
+            </button>
+          </div>
         )}
 
         {showImg === false && (
-          <div className="flex flex-col items-center justify-center gap-y-4 text-xl border border-dashed border-gray-600 w-100 h-80 text-white ">
+          <div className="flex flex-col items-center justify-center gap-y-4 text-xl border border-dashed border-gray-600 w-[30vw] h-[60vh] text-white ">
             <button className="rounded-2xl bg-blue-500 px-3 py-2 w-32">
               <label For="input">Upload</label>
             </button>
@@ -24,7 +35,7 @@ const Post = () => {
             <input
               onChange={(e) => {
                 setImgUrl(URL.createObjectURL(e.target.files[0]));
-                setShowImg(true)
+                setShowImg(true);
               }}
               className="hidden"
               id="input"
@@ -33,11 +44,6 @@ const Post = () => {
             />
           </div>
         )}
-
-        <button onClick={()=>{
-            setShowImg(false)
-            setImgUrl("")
-        }} className="rounded-2xl bg-red-500 px-3 py-2 w-35 text-white mt-4"> Remove image</button>
       </div>
     </>
   );
